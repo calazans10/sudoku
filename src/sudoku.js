3202ed5d59598e55ef70eddaead6f013bb07465d
Jeferson Farias Calazans <calazans10@gmail.com>
2016-05-12 09:51:19 -0300

    Add fixtures to improve tests

--- src/sudoku.js
+++ src/sudoku.js
@@ -1,69 +1,108 @@
 'use strict';
 
-class Sudoku {
-  constructor (board = []) {
+class InvalidBoardError extends Error {
+  constructor(message) {
+    super();
+    this.name = 'InvalidBoardError';
+    this.message = message;
+    this.stack = new Error().stack; // Optional
+  }
+}
+
+class Position {
+  constructor(x, y, board) {
+    this.x = x;
+    this.y = y;
+    this.board = board;
+  }
+}
+
+class Board {
+  getValueByPosition(position) {
+    return this.board[position.x][position.y];
+  }
+
+  positions() {
+    return this
+      .permutation(this.board.length - 1, this.board.length - 1)
+      .map(arr => new Position(arr[0], arr[1]));
+  }
+
+  constructor(board = []) {
     this.board = board;
 
     if (this.board.length && !this.isValidBoard()) {
-      throw new Error('Error!');
+      throw new InvalidBoardError('Board is invalid.');
     }
   }
 
-  numbers() {
-    return [...Array(this.board.length + 1).keys()];
+  resolve() {
+    return this.getSolutions()[0];
   }
 
-  squareRoot() {
-    return Math.sqrt(this.board.length);
+  getSolutions(solutions = []) {
+    if (this.isCompleted()) {
+      solutions.push(this.board);
+      return;
+    }
+
+    let position = this.nextMissing();
+    let availableNumbers = this.availableNumbers(position[0], position[1]);
+
+    if (availableNumbers.length == 0) {
+      return;
+    }
+
+    for (let i = 0; i < availableNumbers.length; i++) {
+      let sudokuClone = this.clone();
+      sudokuClone.board[position[0]][position[1]] = availableNumbers[i];
+      sudokuClone.getSolutions(solutions);
+    }
+
+    return solutions;
   }
 
-  isValidBoard() {
-    let numbers = this.numbers();
-    let positions = this.permutation(this.board.length - 1 , this.board.length - 1);
-
-    for (let position of positions) {
-      if (typeof this.board[position[0]][position[1]] !== 'number' ||
-          numbers.indexOf(this.board[position[0]][position[1]]) === -1) {
+  isCompleted() {
+    for (let position of this.positions()) {
+      if (this.getValueByPosition(position) === 0) {
         return false;
       }
     }
-
     return true;
   }
 
-  arrayIntersection(arr1, arr2) {
-    return arr1
-              .filter(value => arr2.indexOf(value) === -1)
-              .concat(arr2.filter(value => arr1.indexOf(value) === -1));
-  }
-
-  cloneObject(object) {
-    return JSON.parse(JSON.stringify(object));
-  }
-
-  permutation(i, j) {
-    let permutation = [];
-
-    for (var k = 0; k <= i; k++) {
-      for (var l = 0; l <= j; l++) {
-        permutation.push([k, l]);
+  isValid() {
+    let regions = this.regions();
+    for (let x in regions) {
+      if (!this.isValidRegion(regions[x])) {
+        return false;
       }
     }
-
-    return permutation;
+    return true;
   }
 
-  pivots() {
-    let pivots = [];
-    let sqrt = this.squareRoot();
-
-    for (let i = 0; i < this.board.length; i += sqrt) {
-      for (let j = 0; j < this.board.length; j += sqrt) {
-        pivots.push([i, j]);
+  nextMissing() {
+    for (let i = 0; i < this.board.length; i++) {
+      for (let j = 0; j < this.board.length; j++) {
+        if (this.board[i][j] === 0) {
+          return [i, j];
+        }
       }
     }
+    return null;
+  }
 
-    return pivots;
+  availableNumbers(x, y) {
+    let numbers = this.numbers().slice(1);
+    let relatedNumbers = [];
+
+    this.related(x, y).forEach(position => {
+      if (this.board[position[0]][position[1]] !== 0) {
+        relatedNumbers.push(this.board[position[0]][position[1]]);
+      }
+    });
+
+    return this.arrayIntersection(numbers, relatedNumbers);
   }
 
   lines() {
@@ -117,9 +156,8 @@ class Sudoku {
     return this.lines().concat(this.columns(), this.squares());
   }
 
-  relatedPivot(x, y) {
-    let sqrt = this.squareRoot();
-    return [x - (x % sqrt), y - (y % sqrt)];
+  related(x, y) {
+    return this.relatedByLine(x, y).concat(this.relatedByColumn(x, y), this.relatedBySquare(x, y));
   }
 
   relatedByLine(x, y) {
@@ -137,28 +175,64 @@ class Sudoku {
     return this.squares()[index];
   }
 
-  related(x, y) {
-    return this.relatedByLine(x, y).concat(this.relatedByColumn(x, y), this.relatedBySquare(x, y));
+  relatedPivot(x, y) {
+    let sqrt = this.squareRoot();
+    return [x - (x % sqrt), y - (y % sqrt)];
   }
 
-  isCompleted() {
-    for (let x in this.board) {
-      for (let y in this.board) {
-        if (this.board[x][y] === 0) {
-          return false;
-        }
+  pivots() {
+    let pivots = [];
+    let sqrt = this.squareRoot();
+
+    for (let i = 0; i < this.board.length; i += sqrt) {
+      for (let j = 0; j < this.board.length; j += sqrt) {
+        pivots.push([i, j]);
       }
     }
-    return true;
+
+    return pivots;
   }
 
-  isValid() {
-    let regions = this.regions();
-    for (let x in regions) {
-      if (!this.isValidRegion(regions[x])) {
+  arrayIntersection(arr1, arr2) {
+    return arr1
+      .filter(value => arr2.indexOf(value) === -1)
+      .concat(arr2.filter(value => arr1.indexOf(value) === -1));
+  }
+
+  permutation(i, j) {
+    let permutation = [];
+
+    for (var k = 0; k <= i; k++) {
+      for (var l = 0; l <= j; l++) {
+        permutation.push([k, l]);
+      }
+    }
+
+    return permutation;
+  }
+
+  numbers() {
+    return [...Array(this.board.length + 1).keys()];
+  }
+
+  squareRoot() {
+    return Math.sqrt(this.board.length);
+  }
+
+  clone() {
+    return new Board(JSON.parse(JSON.stringify(this.board)));
+  }
+
+  isValidBoard() {
+    let numbers = this.numbers();
+    let positions = this.permutation(this.board.length - 1 , this.board.length - 1);
+
+    for (let position of positions) {
+      if (numbers.indexOf(this.board[position[0]][position[1]]) === -1) {
         return false;
       }
     }
+
     return true;
   }
 
@@ -172,56 +246,6 @@ class Sudoku {
     }
     return true;
   }
-
-  nextMissing() {
-    for (let i = 0; i < this.board.length; i++) {
-      for (let j = 0; j < this.board.length; j++) {
-        if (this.board[i][j] === 0) {
-          return [i, j];
-        }
-      }
-    }
-    return null;
-  }
-
-  availableNumbers(x, y) {
-    let numbers = this.numbers().slice(1);
-    let relatedNumbers = [];
-
-    this.related(x, y).forEach(position => {
-      if (this.board[position[0]][position[1]] !== 0) {
-        relatedNumbers.push(this.board[position[0]][position[1]]);
-      }
-    });
-
-    return this.arrayIntersection(numbers, relatedNumbers);
-  }
-
-  getSolutions(solutions = []) {
-    if (this.isCompleted()) {
-      solutions.push(this.board);
-      return;
-    }
-
-    let position = this.nextMissing();
-    let availableNumbers = this.availableNumbers(position[0], position[1]);
-
-    if (availableNumbers.length == 0) {
-      return;
-    }
-
-    for (let i = 0; i < availableNumbers.length; i++) {
-      let sudokuClone = new Sudoku(this.cloneObject(this.board));
-      sudokuClone.board[position[0]][position[1]] = availableNumbers[i];
-      sudokuClone.getSolutions(solutions);
-    }
-
-    return solutions;
-  }
-
-  resolve() {
-    return this.getSolutions()[0];
-  }
 }
 
-export default Sudoku;
+export default Board;
