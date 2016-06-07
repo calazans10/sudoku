185f1dfe1944683f6f499d37ac3e79d6473a1e37
Jeferson Farias Calazans <calazans10@gmail.com>
2016-06-07 11:44:31 -0300

    Add code to generate sudoku on the command line

--- src/clients/cli.js
+++ src/clients/cli.js
@@ -3,10 +3,31 @@
 
 const Board = require('../board');
 const program = require('commander');
+const Table = require('cli-table');
+
+function newSoduku() {
+  let board = Board.generate();
+  let count = 0;
+  let line = [];
+  let table = new Table();
+
+  for (let position of board.positions) {
+    if (count === board.board.length) {
+      table.push(line);
+      line = [];
+      count = 0;
+    } else {
+      line.push(position.value);
+      count++;
+    }
+  }
+
+  return table.toString();
+}
 
 program
   .version('0.0.1')
-  .option('-g, --generate', 'output a initial sudoku', Board.generate)
+  .option('-n, --new', 'output a new sudoku', newSoduku)
   .parse(process.argv);
 
-console.log(' board: %j', program.generate);
+console.log('%s', program.new);
